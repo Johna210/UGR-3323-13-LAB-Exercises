f3de685ee96a0aeeba1fcd02e57f9c71dc200b5b
Johna210 <solomonjohna21@gmail.com>
2024-04-05 16:41:28 +0300

    Third Screen:

--- LAB2Exercises/lab2/lib/main.dart
+++ LAB2Exercises/lab2/lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:lab2/screen1.dart';
 import 'package:lab2/screen2.dart';
+import 'package:lab2/screen3.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Screen2(),
+      home: Screen3(),
     ),
   );
 }
