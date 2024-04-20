c16ce9a511f95e84a984ee66f446944b438bacea
Johna210 <solomonjohna21@gmail.com>
2024-04-20 22:47:40 +0300

    Riverpod appstate management

---
+++ LAB3Exercises/riverpod_appstate_management/lib/counter_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CounterNotifier extends StateNotifier<int> {
+  CounterNotifier() : super(0);
+
+  void incrementCounter() {
+    state++;
+  }
+
+  void decrementCounter() {
+    state--;
+  }
+}
+
+final counterProvider = StateNotifierProvider<CounterNotifier, int>(
+  (ref) => CounterNotifier(),
+);
